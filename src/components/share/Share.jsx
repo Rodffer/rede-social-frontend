83c0c69eea9715647050fffb99fd46871c877119
Rayron Rodffer <r.rodffer@gmail.com>
2022-01-30 18:35:20 -0300

    Feat: adicionado estilizacao do componente de compartilhar

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './share.css';
 
-import { PermMedia } from '@material-ui/icons';
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons';
 
 export default function Share() {
   return (
     <div className='share'>
       <div className="shareWrapper">
         <div className="shareTop">
-          {/* <img className='shareProfileImg' src="/assets/person/6.jpeg" alt="" /> */}
+          <img className='shareProfileImg' src="/assets/person/6.jpeg" alt="" />
           <input className='shareInput' placeholder='O que está pensando, hoje ?'/>
         </div>
         <hr className='shareHr'/>
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia className='shareIcon' />
+              <PermMedia htmlColor='tomato' className='shareIcon' />
               <span className='shareOptionText'>Foto ou vídeo</span>
+            </div>
+            <div className="shareOption">
+              <Label htmlColor='blue' className='shareIcon' />
+              <span className='shareOptionText'>Enquete</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor='green' className='shareIcon' />
+              <span className='shareOptionText'>Localização</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor='goldenrod' className='shareIcon' />
+              <span className='shareOptionText'>Sentimento</span>
             </div>
           </div>
         </div>
