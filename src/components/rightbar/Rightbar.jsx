b6fe0408597f675a2a4300365f2ab7c1504c4e5e
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-03 22:38:10 -0300

    Feat: adicionado barra lateral direita

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,44 @@ import './rightbar.css'
 export default function Rightbar() {
   return(
     <div className='rightbar'>
-      Rightbar
+      <div className="rihtbarWrapper">
+        <div className="birthdayContainer">
+          <img  className='birthdayImg' src="/assets/gift.png" alt="" />
+          <span className='birthdayText' > <b>Maria Luz</b> e <b>outros 2</b> amigos </span>
+        </div>
+        <img className='rightbarAd' src="/assets/ads.png" alt="" />
+        <h4 className='rightbarTitle'>Amigos online</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="/assets/person/3.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Lucas Rodrigues</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="/assets/person/2.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Geraldo Rivier</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="/assets/person/1.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Annie Timb</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="/assets/person/4.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Maria Luz</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
