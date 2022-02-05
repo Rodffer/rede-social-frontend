baba74f1b21778548ffe5f2b3bbe48792dc9021a
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-05 15:54:32 -0300

    Feat: adicionado roteamento no app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Home from "./pages/home/Home";
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
 
 function App() {
   return (
-    <>
-    <Home />
-    </>
-    )
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/profile" element={<Profile/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+        {/* <Route path="*" element={<NotFound/>}/> */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
